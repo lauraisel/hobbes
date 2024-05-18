480e577c33a150d26896ce527f114053460ac182
Laura Isel Bacoka Klikic <laura.isel@gmail.com>
2024-05-18 12:47:58 +0200

    Initial commit

---
+++ src/main/java/hr/algebra/hobbes/HobbesApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.hobbes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/algebra/hobbes/HobbesApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.hobbes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
