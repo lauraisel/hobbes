1e24d5d47ae5e1e3c2db2729ac50b81d833d3c83
Laura Isel <laura.isel@gmail.com>
2024-05-25 03:15:55 +0200

    Created Token class with m-1 relationship to User and TokenRepository interface with findByToken

---
+++ src/main/java/hr/algebra/hobbes/repository/TokenRepository.java
@@ -0,0 +1,11 @@
+package hr.algebra.hobbes.repository;
+
+import hr.algebra.hobbes.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findByToken(String token);
+}
