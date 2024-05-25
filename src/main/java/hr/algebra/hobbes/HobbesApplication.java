a24a538e4739bd62ebb05177e165afe42ef9fbe5
Laura Isel <laura.isel@gmail.com>
2024-05-25 09:43:26 +0200

    Added role initialization in the repository

--- src/main/java/hr/algebra/hobbes/HobbesApplication.java
+++ src/main/java/hr/algebra/hobbes/HobbesApplication.java
@@ -1,7 +1,11 @@
 package hr.algebra.hobbes;
 
+import hr.algebra.hobbes.model.Role;
+import hr.algebra.hobbes.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,4 +18,14 @@ public class HobbesApplication {
 		SpringApplication.run(HobbesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if(roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	};
 }
