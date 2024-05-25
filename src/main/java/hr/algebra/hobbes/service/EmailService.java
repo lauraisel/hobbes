f3e99035ec46e8d61e2faf36c54eb04139da3210
Laura Isel <laura.isel@gmail.com>
2024-05-25 09:42:56 +0200

    Refactored EmailService into an interface and its implementation class EmailServiceImpl

--- src/main/java/hr/algebra/hobbes/service/EmailService.java
+++ src/main/java/hr/algebra/hobbes/service/EmailService.java
@@ -2,63 +2,12 @@ package hr.algebra.hobbes.service;
 
 import hr.algebra.hobbes.enums.EmailTemplateNameEnum;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-
-    private  final JavaMailSender mailSender;
-    private final SpringTemplateEngine templateEngine;
-
-    @Async
-    public void sendEmail(
-            String to,
-            String username,
-            EmailTemplateNameEnum emailTemplateName,
-            String confirmationUrl,
-            String activationToken,
-            String subject
-    ) throws MessagingException {
-        String templateName;
-        if (emailTemplateName == null){
-            templateName = "confirm-email";
-        } else {
-            templateName = emailTemplateName.name();
-        }
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,
-                MimeMessageHelper.MULTIPART_MODE_MIXED,
-                StandardCharsets.UTF_8.name()
-        );
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("username",username);
-        properties.put("confirmationUrl",confirmationUrl);
-        properties.put("activationToken",activationToken);
-
-        Context context = new Context();
-        context.setVariables(properties);
-
-        mimeMessageHelper.setFrom("hobbes.theproject@gmail.com");
-        mimeMessageHelper.setTo(to);
-        mimeMessageHelper.setSubject(subject);
-
-        String template = templateEngine.process(templateName, context);
-
-        mimeMessageHelper.setText(template, true);
-
-        mailSender.send(mimeMessage);
-
-    }
+public interface EmailService {
+    void sendEmail(String to,
+                   String username,
+                   EmailTemplateNameEnum emailTemplateName,
+                   String confirmationUrl,
+                   String activationToken,
+                   String subject) throws MessagingException;
 }
