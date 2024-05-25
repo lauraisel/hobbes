a4796b5dadccb51556e5f570c39261ff754d58f0
Laura Isel <laura.isel@gmail.com>
2024-05-25 07:00:03 +0200

    Added annotation for Async due to EmailService

--- src/main/java/hr/algebra/hobbes/HobbesApplication.java
+++ src/main/java/hr/algebra/hobbes/HobbesApplication.java
@@ -3,9 +3,11 @@ package hr.algebra.hobbes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class HobbesApplication {
 
 	public static void main(String[] args) {
